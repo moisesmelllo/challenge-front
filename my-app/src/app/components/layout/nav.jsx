47b074cd0c1a16f704cb22a5086c0fa263203af5
Moises Melo Viana Ferreira <moisesmvferreira@gmail.com>
2023-10-12 15:34:14 -0300

    media

--- my-app/src/app/components/layout/nav.jsx
+++ my-app/src/app/components/layout/nav.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+
 
 const Nav = () => {
   return (
-    <div>users_like.me</div>
+    <>
+    <div className='absolute w-screen z-10 bg-[#8722f3] p-8 shadow-2xl text-white'>
+      <p>users_like.me</p>
+    </div>
+    <div className='flex justify-center items-start bg-[#9022F3] text-3xl pb-32 pt-32 text-white'>
+        <p>Find new users like you</p>
+      </div>
+    </>
   )
 }
 
