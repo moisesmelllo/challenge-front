1f068272947237f566b393f261b5ca54bb59f58f
Moises Melo Viana Ferreira <moisesmvferreira@gmail.com>
2023-10-12 15:34:37 -0300

    body content

--- my-app/src/app/components/layout/body.jsx
+++ my-app/src/app/components/layout/body.jsx
@@ -1,8 +1,18 @@
+import Main from '@/app/components/body/Main'
 import React from 'react'
+import Personal from '../body/Personal'
+import Contact from '../body/Contact'
 
 const Body = () => {
   return (
-    <div>body</div>
+    <div className='relative flex flex-col items-center mx-auto -top-28
+    h-[40rem] border border-black w-2/3 bg-white'>
+      <Main />
+      <div className='flex mt-64 h-[100%] w-[100%]'>
+        <Personal />
+        <Contact />
+      </div>
+    </div>
   )
 }
 
