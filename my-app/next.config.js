4a41dae03fdc15302a6c63f5d2ae2ddc32865a55
Moises Melo Viana Ferreira <moisesmvferreira@gmail.com>
2023-10-13 21:47:09 -0300

    configs

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'https://randomuser.me/api/',
+        port: '',
+        pathname: 'https://randomuser.me/api/**'
+      }
+    ],
+    domains: ['randomuser.me'],
+  }
+}
 
 module.exports = nextConfig
