e17de0b614b0ba74780bbf520f92e145c2189447
Moises Melo Viana Ferreira <moisesmvferreira@gmail.com>
2023-10-14 18:48:00 -0300

    fixing css personal page

--- my-app/src/app/components/body/Personal.jsx
+++ my-app/src/app/components/body/Personal.jsx
@@ -4,7 +4,7 @@ import { ChevronDown } from 'lucide-react';
 const Personal = ({userData}) => {
   return (
     <div className='flex flex-col'>
-      <div className='space-y-4 p-4'>
+      <div className='space-y-4 p-4 h-40'>
         <p className='font-bold text-xl mt-2'>Personal info</p>
         <p>born at: {userData.nat}</p>
         <p>age: {userData.registered.age} years old</p>
