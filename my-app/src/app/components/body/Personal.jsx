c8753435c976714cbc85900530b71f4031be2aea
Moises Melo Viana Ferreira <moisesmvferreira@gmail.com>
2023-10-13 21:45:20 -0300

    Personal info component

---
+++ my-app/src/app/components/body/Personal.jsx
@@ -0,0 +1,21 @@
+
+import { ChevronDown } from 'lucide-react';
+
+const Personal = ({userData}) => {
+  return (
+    <div className='flex flex-col'>
+      <div className='space-y-4 p-4'>
+        <p className='font-bold text-xl mt-2'>Personal info</p>
+        <p>born at: {userData.nat}</p>
+        <p>age: {userData.registered.age} years old</p>
+      </div>
+      <hr className='border border-gray-200'/>
+      <div className='flex px-4 py-3 space-x-2 items-center cursor-pointer text-[#6da7ad] font-bold'>
+        <ChevronDown />
+        <p className='mb-1'>see more</p>
+      </div>
+    </div>
+  )
+}
+
+export default Personal
