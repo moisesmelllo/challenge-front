cae3ddd004d142bc7ecb04d097abe573d0ed4d53
Moises Melo Viana Ferreira <moisesmvferreira@gmail.com>
2023-10-14 18:48:21 -0300

    simplifyng body page

--- my-app/src/app/components/layout/body.jsx
+++ my-app/src/app/components/layout/body.jsx
@@ -4,39 +4,23 @@ import Main from '@/app/components/body/Main'
 import Personal from '../body/Personal'
 import Contact from '../body/Contact'
 
-import { useState, useEffect } from 'react'
+import { useContext } from 'react'
+import { CounterContext } from '../utils/ContextApiProvider'
 
-const url = 'https://randomuser.me/api/'
+const Body = ({onFollow}) => {
+  const {userData, handleClick} = useContext(CounterContext)
 
-const Body = () => {
 
-  const [userData, setUserData] = useState(null)
-
-  async function fetchData() {
-      const res = await fetch(url)
-      const data = await res.json()
-
-      setUserData(data.results[0])
-    }
-
-    useEffect(() => {
-      fetchData()
-    }, [])
+  if (!userData) {
+    return <div>Carregando...</div>;
     
-    function handleClick() {
-      fetchData();
-    }
-
-    if (!userData) {
-      return <div>Carregando...</div>;
-      
-    }
+  }
 
   return (
     <div className='w-2/3 mx-auto'>
       <div className='relative flex flex-col -top-28 shadow border border-gray-400'
       >
-        <Main click={handleClick} userData={userData} />
+        <Main click={handleClick} userData={userData} onFollow={onFollow} />
       </div>
 
       <div className='flex relative bg-white -top-28 mt-1 space-x-1'>
