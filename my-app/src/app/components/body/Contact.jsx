53d5d97acf629c03b909a711f45769a7cd5f49e4
Moises Melo Viana Ferreira <moisesmvferreira@gmail.com>
2023-10-14 18:47:08 -0300

    fixing css contact page

--- my-app/src/app/components/body/Contact.jsx
+++ my-app/src/app/components/body/Contact.jsx
@@ -3,7 +3,7 @@ import { ChevronDown } from 'lucide-react';
 const Contact = ({userData}) => {
   return (
     <div className='flex flex-col'>
-      <div className='space-y-4 p-4'>
+      <div className='space-y-4 p-4 h-40'>
         <p className='font-bold text-xl mt-2'>Contact info</p>
         <p className='hidden sm:block whitespace-pre-line break-words overflow-clip'>email: {userData.email}</p>
         <p>phone 1: {userData.phone}</p>
@@ -11,7 +11,7 @@ const Contact = ({userData}) => {
       <hr className='border border-gray-200'/>
       <div className='flex px-4 py-3 space-x-2 items-center cursor-pointer text-[#6da7ad] font-bold'>
         <ChevronDown />
-        <p className='mb-1'>see more</p>
+        <p className='mb-1 mw-8'>see more</p>
       </div>
     </div>
   )
